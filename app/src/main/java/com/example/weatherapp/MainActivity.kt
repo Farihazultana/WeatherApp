f863af9097b8db0c5eaa3f61e22f2285f7232830
Fariha Sultana <farihazultana@gmail.com>
2023-02-01 22:46:51 +0600

    onPreExecute(), doInBackground(), onPostExecute() functionalities are done in AsyncTask() which is deprecated in Java

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,106 @@
 package com.example.weatherapp
 
+import android.net.wifi.rtt.CivicLocationKeys.CITY
+import android.os.AsyncTask
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import com.example.weatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var city: String = "Dhaka, BD"
+    var API: String = "08ee7b0842cb82b1a2628af4a57dc705"
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        weatherTask.execute()
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            binding.loader.visibility = View.VISIBLE
+            binding.mainContainer.visibility = View.GONE
+            binding.errorText.visibility = View.GONE
+        }
+
+        @RequiresApi(Build.VERSION_CODES.Q)
+        override fun doInBackground(vararg p0: String?): String {
+            var response: String? = try {
+                URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API")
+                    .readText(Charsets.UTF_8)
+            } catch (e: java.lang.Exception) {
+                null
+            }
+            return response!!
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                val jsonObject = JSONObject(result)
+                val main = jsonObject.getJSONObject("main")
+                val sys = jsonObject.getJSONObject("sys")
+                val wind = jsonObject.getJSONObject("wind")
+                val weather = jsonObject.getJSONArray("weather").getJSONObject(0)
+                val updateAt: Long = jsonObject.getLong("dt")
+                val updatedAtText =
+                    "Updated at : " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
+                        Date()
+                    )
+                val temp = main.getString("temp") + "℃"
+                val tempMin = "Min Temp: " + main.getString("temp_min") + "℃"
+                val tempMax = "Max Temp: " + main.getString("temp_max") + "℃"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+                val sunrise: Long = sys.getLong("sunrise")
+                val sunset: Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val address = jsonObject.getString("name") + " , " + sys.getString("country")
+
+                binding.address.text = address
+                binding.updatedAt.text = updatedAtText
+                binding.status.text = weatherDescription.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(
+                        Locale.getDefault()
+                    ) else it.toString()
+                }
+                binding.temp.text = temp
+                binding.tempMin.text = tempMin
+                binding.tempMax.text = tempMax
+                binding.sunrise.text =
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise * 1000))
+                binding.sunset.text =
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset * 1000))
+                binding.wind.text = windSpeed
+                binding.pressure.text = pressure
+                binding.humidity.text = humidity
+
+                //disable progressbar and enable main container
+                binding.loader.visibility = View.GONE
+                binding.mainContainer.visibility = View.VISIBLE
+
+
+            } catch (e: Exception) {
+                binding.loader.visibility = View.GONE
+                binding.errorText.visibility = View.VISIBLE
+            }
+
+        }
     }
 }
